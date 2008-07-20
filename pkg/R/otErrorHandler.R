d784cc6f18460c95f88a261a6fdc8f7e41f0d51f
bodanker (Joshua Ulrich) <josh.m.ulrich@gmail.com>
2008-07-20 14:14:59 +0000

    - A few changes in order to pass R CMD check
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/opentick@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/otErrorHandler.R
+++ pkg/R/otErrorHandler.R
@@ -7,19 +7,19 @@
 function(x) {
 
   # Error Handling
-  if(header$commandStatus==2) {
-    error <- 'r(198);'
-    return(error)
-    error <- list()
-    error$errorCode <- readBin(res$body[1:2], integer(), size=2); loc <- 2
-    error$errorDescLen <- readBin(res$body[-c(1:loc)], integer(), size=2);loc <- loc + 2
-    error$errorDesc <- readBin(res$body[-c(1:loc)], character(), size=resBody$errorDescLen)
-    error <- unlist(reqBody, use.names=FALSE)
-    return(error)
-  }
+#  if(header$commandStatus==2) {
+#    error <- 'r(198);'
+#    return(error)
+#    error <- list()
+#    error$errorCode <- readBin(res$body[1:2], integer(), size=2); loc <- 2
+#    error$errorDescLen <- readBin(res$body[-c(1:loc)], integer(), size=2);loc <- loc + 2
+#    error$errorDesc <- readBin(res$body[-c(1:loc)], character(), size=resBody$errorDescLen)
+#    error <- unlist(reqBody, use.names=FALSE)
+#    return(error)
+#  }
 
-  if(header$commandType==OT$INT_UNKNOWN) {
-    stop('Unknown Command')
-  }
-  return(body)
+#  if(header$commandType==OT$INT_UNKNOWN) {
+#    stop('Unknown Command')
+#  }
+  return(x)
 }
